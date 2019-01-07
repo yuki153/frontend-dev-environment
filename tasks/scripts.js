14a69ece7a6ff46c5ce3a2ff971f422d72920762
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-07 20:41:24 +0900

    modify: Delete options of babel-loader. Separated into '.babelrc'.

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -85,22 +85,11 @@ module.exports = function transpileScript(done) {
         } : {},
         {
           test: /\.js$/,
+          exclude: /node_modules/,
           use: [
               {
               // Babel を利用する
               loader: 'babel-loader',
-              // Babel のオプションを指定する
-              options: {
-                presets: [
-                  // env を指定することで、ES2017 を ES5 に変換。
-                  // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
-                  // webpack の Tree Shaking 機能が使えない
-                  ['@babel/preset-env', {
-                    'modules': false
-                  }]
-                ],
-                plugins: ['@babel/plugin-transform-runtime'],
-              }
             },
           ]
         },
