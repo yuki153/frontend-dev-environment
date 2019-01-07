eebc077eb998f6836433870c898e82a3c9bba64f
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-07 20:40:13 +0900

    modify: Add example text

--- src/project01/js/pc/example.js
+++ src/project01/js/pc/example.js
@@ -16,3 +16,15 @@ const addSmoothScroll = (i) => {
 for (let i = 0; i < smoothScrollTriggers.length; i++) {
     addSmoothScroll(i);
 }
+
+// Use test of 'Promise'
+new Promise(()=>{console.log("promise")})　// eslint-disable-line
+
+// Use test of 'async await'
+async function hoge() {
+    await console.log('aaa');
+}
+hoge();
+
+// Use test of 'Object.entries'
+Object.entries({ foo: 'baz' });
