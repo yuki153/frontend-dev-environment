1ebc672945cfbe8f97002a3ae26b5aee7ce2e2dc
fujino-yuki <xxx153yuu.story@gmail.com>
2019-07-21 01:35:51 +0900

    Improve compilation

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,5 +1,4 @@
 const gulp = require("gulp");
-const uglify = require('gulp-uglify')
 const plumber = require('gulp-plumber')
 const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
@@ -11,48 +10,42 @@ const glob = require('glob');
 const filteringEmptyFile = (paths) => {
   let entryFiles = {};
   let destIgnoredFiles = [];
-  let jsFileCount = 0;
+  let jsFiles = [];
+  let isEntryAll = true;
 
-  const pathsLen = paths.scripts.webpack.length; // ２(PC path / SP path)
-  for (let i = 0; i < pathsLen; i++) {
-    let jsFiles = [];
-    glob.sync(`./${paths.scripts.webpack[i]}`).map(jsFilePath => {
-      jsFiles.push(jsFilePath);
-      jsFileCount += 1;
-    });
-    for (const jsFile of jsFiles) {
-      try {
-        const data = fs.readFileSync(jsFile, 'UTF-8')
-        // jsFile の中身が empty 尚且つ polyfill と名の付くファイルではない時 entryFiles に追加
-        if (data.length && /[a-z]/.test(data) && !/polyfill/.test(jsFile)) {
-          const key = path.basename(jsFile, path.extname(jsFile));
-          if (i === 0) {
-            entryFiles[`pc/${key}`] = jsFile;
-            destIgnoredFiles.push(`!${jsFile}`)
-          }
-          if (i === 1) {
-            entryFiles[`sp/${key}`] = jsFile;
-            destIgnoredFiles.push(`!${jsFile}`)
-          }
+  glob.sync(`./${paths.scripts.src}`).map(jsFilePath => {
+    jsFiles.push(jsFilePath);
+  });
+
+  for (const jsFile of jsFiles) {
+    try {
+      const fileData = fs.readFileSync(jsFile, 'UTF-8')
+      const fileName = path.basename(jsFile, path.extname(jsFile));
+      const fragments = jsFile.split('/');
+      const indexOfFileName = fragments.indexOf(`${fileName}.js`);
+      const isModulesDir = /^modules?$/.test(fragments[indexOfFileName - 1]);
+      const isPluginsDir = /^plugins?$/.test(fragments[indexOfFileName - 1]);
+      const isEmptyFile = !(fileData.length && /[a-z]/.test(fileData));
+      if (false === isEmptyFile && false === isPluginsDir) {
+        if (isModulesDir || indexOfFileName >= 6) {
+          destIgnoredFiles.push(`!${jsFile}`)
+        } else if (isModulesDir) {
+          isEntryAll = false;
+        } else if (indexOfFileName === 4) {
+          entryFiles[fileName] = jsFile;
+          destIgnoredFiles.push(`!${jsFile}`)
+        } else if (indexOfFileName === 5) {
+          entryFiles[`${fragments[4]}/${fileName}`] = jsFile;
+          destIgnoredFiles.push(`!${jsFile}`)
         }
-      } catch(err) {}
-    }
-    // for 文の最後の loop で実行
-    if (i === pathsLen - 1) {
-      const keys = Object.keys(entryFiles);
-      // sp / pc どちらの js ファイルも全て empty または polyfill と名のついたファイルだった時
-      if (keys.length === 0) {
-        gulp.src(paths.scripts.src)
-        .pipe(gulp.dest(paths.scripts.release))
-        return false;
+      } else {
+        isEntryAll = false;
       }
-      // sp / pc の js ファイルが１つ以上でも empty または polyfill と名のついたファイルだった時
-      if (keys.length !== jsFileCount) {
-        gulp.src([paths.scripts.src, ...destIgnoredFiles])
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.scripts.release))
-      }
-    }
+    } catch(err) {console.log(err)}
+  }
+  if (false === isEntryAll) {
+    gulp.src([paths.scripts.src, ...destIgnoredFiles])
+    .pipe(gulp.dest(paths.scripts.release))
   }
   return entryFiles;
 }
